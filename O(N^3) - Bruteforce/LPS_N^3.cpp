aa4ebbadc2821b6557fd2cc19a395696e61d748f
Nikolay Valchanov <158312030+NValchanov09@users.noreply.github.com>
2024-03-05 22:35:36 +0200

    Added LPS_N^3.cpp

---
+++ O(N^3) - Bruteforce/LPS_N^3.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string s;
+int n;
+int lps_left, lps_right;
+
+void read()
+{
+    cin >> n >> s;
+}
+
+bool check(int left, int right)
+{
+    while(left <= right && s[left] == s[right])
+    {
+        left++;
+        right--;
+    }
+    return right < left;
+}
+
+void solve()
+{
+    for(int left = 0; left < n; left++)
+    {
+        for(int right = left; right < n; right++)
+        {
+            if(check(left, right))
+            {
+                int len = right - left + 1;
+                int lps_len = lps_right - lps_left + 1;
+
+                if(lps_len < len)
+                {
+                    lps_left = left;
+                    lps_right = right;
+                }
+            }
+        }
+    }
+}
+
+void print()
+{
+    cout << "Longest Palindromic Substring : " << endl;
+
+    for(int i = lps_left; i <= lps_right; i++)
+    {
+        cout << s[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+    read();
+    solve();
+    print();
+
+	return 0;
+}
